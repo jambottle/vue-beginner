4a2a32a0f890dce2be2db502b9a207dcc6426a5f
jambottle <jaewon.walks@gmail.com>
2021-11-20 08:40:35 +0900

    수강 완료

---
+++ vue-cli-practice/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      str: 'Header',
+    };
+  },
+
+  components: {
+    'app-header': AppHeader,
+  },
+
+  methods: {
+    renewStr: function () {
+      this.str = 'No More Header! 😤';
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue-form-practice/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">ID: </label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">PW: </label>
+      <input id="password" type="password" v-model="password" />
+    </div>
+    <button type="submit">Sign in</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+
+  methods: {
+    submitForm: function () {
+      // event.preventDefault();
+      console.log(this.username, this.password);
+
+      const url = 'https://jsonplaceholder.typicode.com/users';
+      const data = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(url, data)
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
